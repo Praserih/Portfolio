a24dc015a5cd442dfaa678c13d99bf8a9a4331c0
Praserih <48497827+Praserih@users.noreply.github.com>
2019-12-17 20:23:57 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,22 +1,30 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-var height = $(document).height() - $(window).height() -100;
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	var height = $(document).height() - $(window).height() - 100;
 
-    if (scroll >= 250) {
-        $(".toggle").addClass("is-hidden");
-    } else {
-        $(".toggle").removeClass("is-hidden");
-    };
-	
+	if (scroll >= 250) {
+		$(".toggle").addClass("is-hidden");
+	} else {
+		$(".toggle").removeClass("is-hidden");
+	};
+
 	if (scroll >= height) {
-        $(".box").addClass("is-hidden");
-		        $(".contact").addClass("is-hidden");
-   $(".x").addClass("kill");
+		$(".box").addClass("is-hidden");
+		$(".contact").addClass("is-hidden");
+		$(".x").addClass("kill");
 
-    } else {
-        $(".box").removeClass("is-hidden");
-		        $(".contact").removeClass("is-hidden");
-		        $(".x").removeClass("kill");
+	} else {
+		$(".box").removeClass("is-hidden");
+		$(".contact").removeClass("is-hidden");
+		$(".x").removeClass("kill");
+	};
+	
+	if (scroll < height) {
+		$("#about").css({"visibility":"hidden"});
 
-    }
+	} else {
+		$("#about").css({"visibility":"visible"});
+
+	};
+	
 });
