db4e606320fe9469ca13f51ca483d897e89acd2a
Praserih <48497827+Praserih@users.noreply.github.com>
2019-12-17 09:13:29 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+var height = $(document).height() - $(window).height() -100;
+
+    if (scroll >= 250) {
+        $(".toggle").addClass("is-hidden");
+    } else {
+        $(".toggle").removeClass("is-hidden");
+    };
+	
+	if (scroll >= height) {
+        $(".box").addClass("is-hidden");
+		        $(".contact").addClass("is-hidden");
+   $(".x").addClass("kill");
+
+    } else {
+        $(".box").removeClass("is-hidden");
+		        $(".contact").removeClass("is-hidden");
+		        $(".x").removeClass("kill");
+
+    }
+});
